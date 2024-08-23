071fcc3cf2c366dd4c55fb958ed5b0843df13b4e
caroldvarela <carol-dayana-varela@hotmail.com>
2024-08-23 18:50:08 -0500

    Data migration notebook and docstring explanation for db_connection.py

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,9 +1,27 @@
+"""
+This module provides functionality to build a SQLAlchemy engine for connecting
+to a PostgreSQL database using configuration values from environment variables.
+
+It defines the following function:
+- build_engine: Creates and returns a SQLAlchemy engine for the database.
+"""
+
 from decouple import config, UndefinedValueError
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
 def build_engine():
+    """
+    Creates a SQLAlchemy engine for connecting to the PostgreSQL database using
+    configuration values from environment variables.
+
+    Returns:
+        sqlalchemy.engine.base.Engine: The SQLAlchemy engine connected to the database.
+
+    Raises:
+        UndefinedValueError: If any required environment variable is missing.
+        SQLAlchemyError: If there is an error connecting to the database.
+    """
     try:
         dialect = config('PGDIALECT')
         user = config('PGUSER')
@@ -15,17 +33,14 @@ def build_engine():
         print(f"Missing environment variable: {e}")
         raise
 
-    DATABASE_URL = f"{dialect}://{user}:{passwd}@{host}:{port}/{db}"
+    database_url = (f"{dialect}://{user}:{passwd}@{host}:{port}/{db}")
 
     # Test the connection
     try:
-        engine = create_engine(DATABASE_URL)
-        print(f'Successfully connected to the database {db}!')
+        engine = create_engine(database_url)
+        print(f"Successfully connected to the database {db}!")
         return engine
     except SQLAlchemyError as e:
         print(f"Failed to connect to the database: {e}")
-    
 
-    
-    
-    
+
